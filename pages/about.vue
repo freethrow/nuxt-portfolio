9ab9f8f812c2ea8c8e5792f21cc702f1d64cd1e5
Freethrow aka Marko <aleksendric@gmail.com>
2020-12-05 21:57:09 +0100

    pushing pixels...2

--- pages/about.vue
+++ pages/about.vue
@@ -6,17 +6,27 @@
 
     <div v-html=renderedBio></div>
 
-    {{ bio.fields.content }}
+  <form name="contactus" action="/" method="post" netlify netlify-honeypot="bot-field">
+ 
+  <input type="hidden" name="form-name" value="contactus" />
+  <div>  
+    <label for="name">Name:</label> 
+    <input type="text" name="name" required/>
+  </div>
+  <div>
+    <input type="email" name="email" required/>
+    <label for="email">Email:</label>
+  </div>
+  <div>
+    <textarea name="message" required></textarea>
+    <label for="message">Message:</label>
+  </div>
+  <button type="submit" value="Send message">Send</button>
+</form>
 
 
 
-<hr>
-<div class="bg-green-100 h-60">Moksi
 
-<client-only placeholder="Loading...">
-
-<img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Lipofsky_Pippen.jpg" alt="">
-</client-only>
 
 </div>
     
